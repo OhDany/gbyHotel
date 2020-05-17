238fbe02cac616b028051e8728f4f93b9a237a0c
Daniel Ortega <oh.dany.mx@gmail.com>
2020-05-17 11:29:28 -0500

    Make Page Nosotros

--- src/components/contenidoInicio.js
+++ src/components/contenidoInicio.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import Image from 'gatsby-image';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+const TextInicio = styled.div`
+  padding-top: 4rem;
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 3rem;
+  }
+
+  p {
+      line-height: 2;
+    }
+`;
 
 const ContenidoInicio = () => {
 
@@ -27,13 +46,18 @@ const ContenidoInicio = () => {
 
   return (
     <>
-      <h2>{titulo}</h2>
+      <h2
+        css={css`
+          text-align: center;
+          font-size: 4rem;
+          margin-top: 4rem;
+        `}
+      >{titulo}</h2>
 
-      <div>
+      <TextInicio>
         <p>{contenido}</p>
-        {/* <img src={imagen.fluid.src} /> */}
         <Image fluid={imagen.fluid} />
-      </div>
+      </TextInicio>
     </>
   );
 }
