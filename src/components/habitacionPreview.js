c1b2584606ea49703bb16d166e49cd7f5a71cc29
Daniel Ortega <oh.dany.mx@gmail.com>
2020-05-17 12:16:30 -0500

    Query with Hooks

---
+++ src/components/habitacionPreview.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HabitacionPreview = () => {
+  return ( <h2>Desde Habitacion</h2> );
+}
+ 
+export default HabitacionPreview;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import { css } from '@emotion/core';
 import Layout from '../components/layout';
 import ImagenHotel from '../components/imagenHotel';
 import ContenidoInicio from '../components/contenidoInicio';
+import HabitacionPreview from '../components/habitacionPreview';
+import useHabitaciones from '../components/hooks/useHabitaciones';
 
-const IndexPage = () => (
-    <Layout>
-        <ImagenHotel />
-        <ContenidoInicio />
-    </Layout>
-)
+const IndexPage = () => {
+
+    const habitaciones = useHabitaciones();
+
+    console.log(habitaciones);
+
+    return (
+        <Layout>
+            <ImagenHotel />
+            <ContenidoInicio />
+
+            <h2
+              css={css`
+                text-align: center;
+                margin-top: 5rem;
+                font-size: 3rem;
+              `}
+            >Nuestras Habitaciones</h2>
+
+            <ul>
+              {habitaciones.map(habitacion => (
+                <HabitacionPreview />
+              ))}
+            </ul>
+        </Layout>
+    )
+    
+}
 
 export default IndexPage
